2a9e4b49cfba8da69b8842bf667e655f4bbd397e
sofo01 <sofoklis_the@hotmail.com>
2016-09-11 14:25:10 +0100

    Set Up Layout and JS Class

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+var Sketch = function (blocks = 16, color = 'Red') {};
 
-function Sketch(blocks, color) {
-  // if (blocks === undefined)
-  //   blocks = 16;
-  // if (color === undefined)
-  //   color = 'Black';
+Sketch.prototype.add_blocks = function() {
 
-  function add_blocks() {
-    for (var i = 1; i <= blocks; i++) {
-
-    }
-  }
-
-  var clear_blocks = function() {
-
-  }
-
-  var color_switch = function() {
-
-  }
 };
 
+Sketch.prototype.clear_blocks = function() {
+};
 
+Sketch.prototype.color_switch = function() {
+};
+
+Sketch.prototype.get_blocks = function () {
+  return this.blocks;
+};
+
+Sketch.prototype.get_color = function() {
+  return this.color;
+}
+
+var my_sketch = new Sketch();
+console.log(my_sketch.get_blocks());
