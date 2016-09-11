a8251f274c2f635cd649a639e4731af33abf07a4
sofo01 <sofoklis_the@hotmail.com>
2016-09-11 01:09:57 +0100

    Create JS Object

---
+++ script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function Sketch(blocks, color) {
+  // if (blocks === undefined)
+  //   blocks = 16;
+  // if (color === undefined)
+  //   color = 'Black';
+
+  function add_blocks() {
+    for (var i = 1; i <= blocks; i++) {
+
+    }
+  }
+
+  var clear_blocks = function() {
+
+  }
+
+  var color_switch = function() {
+
+  }
+};
+
+
